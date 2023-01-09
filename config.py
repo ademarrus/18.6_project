414cb6511a195eb0050ee850ce12000f2f792fc3
ademarrus <119788923+ademarrus@users.noreply.github.com>
2023-01-09 23:21:39 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = '5915903424:AAFBnxmQRqlW0w7jixy5UBCuhzu4sYL9BjE'
+
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD'
+}
+symbols = {
+    'EUR' : '€',
+    'RUB' : '₽',
+    'USD' : '$'
+}
